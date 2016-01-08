42ba641d26b5fe4e64367771fe60083c517fdcb1
wjc133 <wjc133@sina.com>
2016-01-08 12:20:14 +0800

    不能编译的

---
+++ findmyphone-library/HttpVisitor/src/main/java/com/elite/findmyphone/httpvisitor/RequestQueue.java
@@ -0,0 +1,72 @@
+package com.elite.findmyphone.httpvisitor;
+
+import com.android.volley.NetworkDispatcher;
+import com.android.volley.Request;
+import com.android.volley.ResponseDelivery;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Create by wjc133
+ * Date: 2016/1/7
+ * Time: 11:27
+ */
+public class RequestQueue {
+    private AtomicInteger mSequenceGenerator = new AtomicInteger();
+    private PriorityBlockingQueue<Request<?>> mNetworkQueue = new PriorityBlockingQueue<>();
+    private NetWorker mNetWorker;
+    private NetworkDispatcher[] mDispathers;
+    // TODO: 2016/1/7 最后要把请求换成自己的
+    private final Set<Request<?>> mCurrentRequests = new HashSet<>();
+
+    private static final int DEFAULT_NETWORK_THREAD_POOL_SIZE = 4;
+
+    public RequestQueue(NetWorker worker, int threadPoolSize) {
+        this.mNetWorker = worker;
+        this.mDispathers = new NetworkDispatcher[threadPoolSize];
+    }
+
+    public RequestQueue(NetWorker worker) {
+        this(worker, DEFAULT_NETWORK_THREAD_POOL_SIZE);
+    }
+
+    public <T> void add(Request<T> request) {
+//        request.setRequestQueue(this);
+        synchronized (mCurrentRequests) {
+            mCurrentRequests.add(request);
+        }
+
+        //给request编序号
+        request.setSequence(getSquenceNumber());
+    }
+
+    public void start() {
+        //有没停止的任务先全都停掉
+        stop();
+        for (int i = 0; i < mDispathers.length; i++) {
+            NetworkDispatcher networkDispatcher = new NetworkDispatcher(mNetworkQueue, mNetWorker, ResponseDelivery delivery);
+            mDispathers[i] = networkDispatcher;
+            networkDispatcher.start();
+        }
+    }
+
+    public void stop() {
+        for (int i = 0; i < mDispathers.length; i++) {
+            if (mDispathers[i] != null) {
+                mDispathers[i].quit();
+            }
+        }
+    }
+
+    /**
+     * 为请求生成一个序列号
+     *
+     * @return 序列号
+     */
+    private int getSquenceNumber() {
+        return mSequenceGenerator.incrementAndGet();
+    }
+}
